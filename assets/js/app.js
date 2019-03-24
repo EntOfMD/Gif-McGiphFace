5861352a04e0a49aec600ecf16776b7d5ab2ccf0
Ashell Fernando <ashellz2010@gmail.com>
2019-03-24 01:40:54 -0400

    data receives but is getting a CORB/CORS error

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,69 @@ let topics = [
     'lions'
 ];
 
+function Gif(id, rating, title, url, still, original) {
+    this.id = id;
+    this.rating = rating;
+    this.title = title;
+    this.url = url;
+    // this.images['original'].url = original;
+    // this.iamges['480w_still'].url = still;
+}
+
+const variables = {
+    gif_meta: []
+};
 const fx = {
+    ConstructGifs: data => {
+        for (i = 0; i < data.length; i++) {
+            var gif_data = new Gif(
+                data[i].id,
+                data[i].rating,
+                data[i].title,
+                data[i].url
+                // data[i].still,
+                // data[i].original
+            );
+            // variables.gif_meta = Object.entries(gif_data);
+            // console.log(variables.gif_meta);
+            console.log(gif_data);
+            let gif_img = $('<img>');
+            gif_img.attr('src', gif_data.url);
+            $('#gif_display').prepend(gif_img);
+        }
+    },
+    fetchGifs: () => {
+        let q = $(this).attr('data-name');
+        let APIKEY = `OLDkiP8Zn4nD6tLAVIcPIvuYklXx0t6S`;
+
+        let qURL =
+            'https://api.giphy.com/v1/gifs/search?api_key=' +
+            APIKEY +
+            '&q=' +
+            q +
+            '&limit=10&offset=0&rating=PG&lang=en';
+        // console.log(this);
+        $.ajax({
+            url: qURL,
+            method: 'GET',
+            contentType: 'application/json',
+            responseType: 'application/json'
+        }).then(res => {
+            if (res.meta.status === 200) {
+                fx.ConstructGifs(res.data);
+                console.log(
+                    `Successfully fetched data. API MSG: ${res.meta.msg}`
+                );
+            } else {
+                console.log('no');
+            }
+        });
+    },
     renderButtons: () => {
         $('#buttons').empty();
         for (i = 0; i < topics.length; i++) {
             var genBtn = $('<button>');
-            genBtn.addClass('btn btn-outline-info btn-sm mx-1');
+            genBtn.addClass('btn btn-outline-info btn-sm mx-1 user_gen_btn');
             genBtn.attr('data-name', topics[i]);
             genBtn.text(topics[i]);
             $('#buttons').append(genBtn);
@@ -22,8 +79,6 @@ const fx = {
 };
 
 $(function() {
-    //inital rendering of hard-coded topics
-    fx.renderButtons();
     //this clears out the search field when user clicks it
     $('input').focus(() => {
         $('#user_input').val('');
@@ -37,4 +92,8 @@ $(function() {
         topics.push(searchTerm);
         fx.renderButtons();
     });
+    $(document).on('click', '.user_gen_btn', fx.fetchGifs);
+
+    //inital rendering of hard-coded topics
+    fx.renderButtons();
 });
