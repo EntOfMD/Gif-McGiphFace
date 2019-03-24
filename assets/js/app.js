4d3887ab8ebfda283e59abcdb835c354e444ec14
Ashell Fernando <ashellz2010@gmail.com>
2019-03-23 23:35:49 -0400

    +buttons show up -searched or coded, +UI redesign

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,40 @@
-$(document).click(() => {
-    $('#card1').toggle('slow');
+let topics = [
+    'puppies',
+    'cats',
+    'birds',
+    'pigs',
+    'hamsters',
+    'elephants',
+    'lions'
+];
+
+const fx = {
+    renderButtons: () => {
+        $('#buttons').empty();
+        for (i = 0; i < topics.length; i++) {
+            var genBtn = $('<button>');
+            genBtn.addClass('btn btn-outline-info btn-sm mx-1');
+            genBtn.attr('data-name', topics[i]);
+            genBtn.text(topics[i]);
+            $('#buttons').append(genBtn);
+        }
+    }
+};
+
+$(function() {
+    //inital rendering of hard-coded topics
+    fx.renderButtons();
+    //this clears out the search field when user clicks it
+    $('input').focus(() => {
+        $('#user_input').val('');
+    });
+    //WHEN the user clicks this, badabing dababoom, magic happens and puts the search term into the topics Arr and display the topic list
+    $('#user_btn').click(e => {
+        e.preventDefault();
+        let searchTerm = $('#user_input')
+            .val()
+            .trim();
+        topics.push(searchTerm);
+        fx.renderButtons();
+    });
 });
