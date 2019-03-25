7138fa8bc1a8f884c713d104dc7fa6b93a95409d
Ashell Fernando <ashellz2010@gmail.com>
2019-03-25 18:26:43 -0400

    some style changes, +hw reqs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,98 +1,107 @@
 let topics = [
-  'zenyatta',
-  'cats',
-  'birds',
-  'pigs',
-  'hamsters',
-  'elephants',
-  'lions'
+    'zenyatta',
+    'cats',
+    'birds',
+    'pigs',
+    'hamsters',
+    'elephants',
+    'lions'
 ];
 
 function Gif(id, rating, title, url, still, original) {
-  this.id = id;
-  this.rating = rating;
-  this.title = title;
-  this.url = url;
-  // this.images['original'].url = original;
-  // this.iamges['480w_still'].url = still;
+    this.id = id;
+    this.rating = rating;
+    this.title = title;
+    this.url = url;
+    this.still = still;
+    this.original = original;
 }
 
-const variables = {
-  gif_meta: []
-};
 const fx = {
-  ConstructGifs: function(data) {
-    $('#gif_display').empty();
-    for (i = 0; i < data.length; i++) {
-      var gif_data = new Gif(
-        data[i].id,
-        data[i].rating,
-        data[i].title,
-        data[i].images.original.url
-        // data[i].still,
-        // data[i].original
-      );
-      // variables.gif_meta = Object.entries(gif_data);
-      // console.log(variables.gif_meta);
-      // console.log(gif_data.url);
-      let gif_img = $('<img>');
-      gif_img.addClass('mx-3 my-3 tall');
-      gif_img.attr('src', gif_data.url);
-      console.log(gif_data);
-      $('#gif_display').prepend(gif_img);
+    ConstructGifs: function(data) {
+        $('#gif_display').empty();
+        for (i = 0; i < data.length; i++) {
+            var gif_data = new Gif(
+                data[i].id,
+                data[i].rating,
+                data[i].title,
+                data[i].url,
+                data[i].images.fixed_height_still.url,
+                data[i].images.original.url
+            );
+
+            let gif_img = $('<img>');
+            let gif_card = $('<div>');
+
+            gif_card.prepend(`<div class="card" style="width: 18rem;">
+                               `);
+            gif_card.append(gif_img);
+            gif_card.append(` <div class="card-body">
+            <p class="card-text title">${gif_data.title}. Rated: <strong>${
+                gif_data.rating
+            }</strong></p>
+          </div></div>`);
+
+            gif_img.addClass('mx-3 my-3 tall');
+            gif_img.attr('src', gif_data.still);
+            gif_card.addClass('single');
+            $('#gif_display').prepend(gif_card);
+            console.log(data);
+        }
+    },
+    fetchGifs: function() {
+        let q = $(this).attr('data-name');
+
+        let APIKEY = `OLDkiP8Zn4nD6tLAVIcPIvuYklXx0t6S`;
+
+        let qURL =
+            'https://api.giphy.com/v1/gifs/search?api_key=' +
+            APIKEY +
+            '&q=' +
+            q +
+            '&limit=10&offset=0&rating=PG&lang=en';
+
+        $.ajax({
+            url: qURL,
+            method: 'GET',
+            contentType: 'application/json',
+            responseType: 'application/json'
+        }).then(function(res) {
+            if (res.meta.status === 200) {
+                fx.ConstructGifs(res.data);
+                console.log(
+                    `Successfully fetched data. API MSG: ${res.meta.msg}`
+                );
+            } else {
+                console.log('no');
+            }
+        });
+    },
+    renderButtons: function() {
+        $('#buttons').empty();
+        for (i = 0; i < topics.length; i++) {
+            var genBtn = $('<button>');
+            genBtn.addClass('btn btn-outline-danger btn-sm mx-1 user_gen_btn');
+            genBtn.attr('data-name', topics[i]);
+            genBtn.text(topics[i]);
+            $('#buttons').append(genBtn);
+        }
     }
-  },
-  fetchGifs: function() {
-    let q = $(this).attr('data-name');
-
-    let APIKEY = `OLDkiP8Zn4nD6tLAVIcPIvuYklXx0t6S`;
-
-    let qURL =
-      'https://api.giphy.com/v1/gifs/search?api_key=' +
-      APIKEY +
-      '&q=' +
-      q +
-      '&limit=10&offset=0&rating=PG&lang=en';
-
-    $.ajax({
-      url: qURL,
-      method: 'GET',
-      contentType: 'application/json',
-      responseType: 'application/json'
-    }).then(function(res) {
-      if (res.meta.status === 200) {
-        fx.ConstructGifs(res.data);
-        console.log(`Successfully fetched data. API MSG: ${res.meta.msg}`);
-      } else {
-        console.log('no');
-      }
-    });
-  },
-  renderButtons: function() {
-    $('#buttons').empty();
-    for (i = 0; i < topics.length; i++) {
-      var genBtn = $('<button>');
-      genBtn.addClass('btn btn-outline-danger btn-sm mx-1 user_gen_btn');
-      genBtn.attr('data-name', topics[i]);
-      genBtn.text(topics[i]);
-      $('#buttons').append(genBtn);
-    }
-  }
 };
 
 $(function() {
-  //WHEN the user clicks this, badabing dababoom, magic happens and puts the search term into the topics Arr and display the topic list
-  $('#user_btn').click(e => {
-    e.preventDefault();
-    let searchTerm = $('#user_input')
-      .val()
-      .trim();
-    topics.push(searchTerm);
-    fx.renderButtons();
-    $('#user_input').val('');
-  });
-  $(document).on('click', '.user_gen_btn', fx.fetchGifs);
+    //WHEN the user clicks this, badabing dababoom, magic happens and puts the search term into the topics Arr and display the topic list
+    $('#user_btn').click(e => {
+        e.preventDefault();
+        let searchTerm = $('#user_input')
+            .val()
+            .trim();
+        topics.push(searchTerm);
+        fx.renderButtons();
+        $('#user_input').val('');
+    });
+    $(document).on('click', '.user_gen_btn', fx.fetchGifs);
 
-  //inital rendering of hard-coded topics
-  fx.renderButtons();
+    //inital rendering of hard-coded topics
+    fx.renderButtons();
 });
